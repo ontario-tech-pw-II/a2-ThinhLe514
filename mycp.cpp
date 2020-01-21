ec4309bee5079af3ed5af204c093ee589c3a905a
Thinh Le <55011202+ThinhLe514@users.noreply.github.com>
2020-01-21 11:15:20 -0500

    Add files via upload

--- mycp.cpp
+++ mycp.cpp
@@ -1,13 +1,29 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
  	ifstream fin;
  	ofstream fout;
+    string finame, foname;
 	
 	// check if there are enough arguments
-	
+    if (argc < 3)
+    {
+        cerr << " Not enough arguments" << endl;
+    } else if (argc > 3)
+    {
+        cerr << " Too many arguments" << endl;
+    }
+
+    finame = argv[1];
+    foname = argv[2];
 	
 	// open the first file
- 	
+ 	fin.open(finame);
+
 	char c;
 
  	if (fin.fail()) // check if it is successful 
@@ -18,6 +34,7 @@ int main(int argc, char const *argv[])
  	
 
 	// open the second file
+    fout.open(foname);
 	
  	if (fout.fail())
  	{
